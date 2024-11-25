f7a713a63f195fc6fbee482e2060bdaaae5df88a
sammartinoj <sammartinoj1@montclair.edu>
2024-11-25 17:39:20 -0500

    cleaning file

--- cleaning.sh
+++ cleaning.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sed 's/,/ /g'  mental-health-dataset.csv | head
+file=$"mental-health.csv"
+#sed -e 's/\r//g' $file | sed 's/@//g'|cut -f3 -d ','|sort -u|head
+#cut -f2 -d ',' $file > statements
+#cut -f3 -d ',' $file > categories
+
+cat $file | sed -e '/^[[:space:]]*$/d'|tr -s ' '| tr -d '\r' |sed 's/--//g'|sed '/,,/d' > temp-file
+
 
