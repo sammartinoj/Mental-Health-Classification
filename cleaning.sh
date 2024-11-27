61d36d67bd7aa8dacba4d0f4b1991980935a1cc3
sammartinoj <sammartinoj1@montclair.edu>
2024-11-27 12:52:20 -0500

    updates

--- cleaning.sh
+++ cleaning.sh
@@ -2,5 +2,6 @@
 
 file=$"mental-health.csv"
 cat $file | sed -e '/^[[:space:]]*$/d'|tr -s ' '| tr -d '\r' |sed 's/--//g'|sed '/,,/d' | perl -0777 -pe 's/\n([^\d])/\1/g' > bep-bile
-cat bep-bile | cut -f3 -d ','|sort|uniq -c
 
+cat bep-bile | cut -f3 -d ','|sort|uniq -c > disorders
+cat bep-bile | cut -f2 -d ',' > statements
