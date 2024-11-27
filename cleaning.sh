1dfac91e56738ff5499f7a02434c13c3b37cab50
sammartinoj <sammartinoj1@montclair.edu>
2024-11-26 22:52:50 -0500

    finished cleaning sh

--- cleaning.sh
+++ cleaning.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 file=$"mental-health.csv"
-#sed -e 's/\r//g' $file | sed 's/@//g'|cut -f3 -d ','|sort -u|head
-#cut -f2 -d ',' $file > statements
-#cut -f3 -d ',' $file > categories
-
-cat $file | sed -e '/^[[:space:]]*$/d'|tr -s ' '| tr -d '\r' |sed 's/--//g'|sed '/,,/d' > temp-file
-
+cat $file | sed -e '/^[[:space:]]*$/d'|tr -s ' '| tr -d '\r' |sed 's/--//g'|sed '/,,/d' | perl -0777 -pe 's/\n([^\d])/\1/g' > bep-bile
+cat bep-bile | cut -f3 -d ','|sort|uniq -c
 
