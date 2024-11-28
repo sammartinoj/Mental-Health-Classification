35ac280a0d7027b0cf69d0cbfea0d0ed87b747f3
sammartinoj <sammartinoj1@montclair.edu>
2024-11-27 22:16:49 -0500

    updated for the individual values in the solo csvs

--- sorting.sh
+++ sorting.sh
@@ -4,9 +4,9 @@ file="cleaned-output"
 
 while read -r line; do
 	category=$(echo "$line" | cut -f3 -d ',')
-	if [ -s "$category.csv" ]; then
-		echo $line >> $category.csv
+	if [ -s mini-csvs/"$category.csv" ]; then
+		echo $line >> mini-csvs/$category.csv
 	else
-		echo $line > $category.csv
+		echo $line > mini-csvs/$category.csv
 	fi
 done < $file
