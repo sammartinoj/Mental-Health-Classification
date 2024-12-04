2efad7fae7835b3ba27d75bf684d234841bdcd7f
rachelhamelburg <hamelburgr1@montclair.edu>
2024-12-04 11:45:19 -0500

    Modified tagger.py and requirements

--- pos/tagger.py
+++ pos/tagger.py
@@ -11,16 +11,14 @@ except OSError:
     print("Installing en_core_web_sm.")
     subprocess.run(["python3", "-m", "spacy", "download", "en_core_web_sm"], check=True)
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
+input = os.path.expandvars("$HOME/text_tools_project/data/mini-csvs")
+output = os.path.expandvars("$HOME/text_tools_project/data/tagged-csvs")
+os.makedirs(output, exist_ok=True)
 
-input_dir = os.path.join(script_dir, "../mini-csvs")  
-output_dir = "tagged-csvs"
-os.makedirs(output_dir, exist_ok=True)
-
-for file_name in os.listdir(input_dir):
-    file_path = os.path.join(input_dir, file_name)
-    base_name = file_name.replace(".csv", "")
-    output_path = os.path.join(output_dir, f"{base_name}-pos.csv")
+for file_name in os.listdir(input):
+    file_path= os.path.join(input, file_name)
+    basename = file_name.replace(".csv", "")
+    output_path= os.path.join(output, f"{basename}-pos.csv")
 
     df = pd.read_csv(file_path, header=None)
 
@@ -42,5 +40,4 @@ for file_name in os.listdir(input_dir):
 
     tagged_df = pd.DataFrame(results)
     tagged_df.to_csv(output_path, index=False)
-    print(f"Processed {file_name} -> {output_path}")
-
+    print(f"Finished {file_name} : {output_path}")
